7fbe9e67e1727e8e3a8e069773ff06bb306b04b7
phuchit <ripzery@gmail.com>
2016-07-15 19:12:13 +0700

    Add navigation layout

--- app/src/main/java/com/socket9/pointube/screens/MainActivity.kt
+++ app/src/main/java/com/socket9/pointube/screens/MainActivity.kt
@@ -1,25 +1,103 @@
 package com.socket9.pointube.screens
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.socket9.pointube.R
 import com.socket9.pointube.extensions.replaceFragment
 import com.socket9.pointube.extensions.setupToolbar
+import com.socket9.pointube.screens.about.AboutFragment
 import com.socket9.pointube.screens.home.HomeFragment
+import com.socket9.pointube.screens.point.PointFragment
 import com.socket9.pointube.screens.promotion.main.PromotionFragment
+import com.socket9.pointube.screens.setting.SettingFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_toolbar.*
 import org.jetbrains.anko.AnkoLogger
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
 
+    lateinit private var drawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupToolbar()
+
+        drawerToggle = setupDrawerToggle()
+        drawerLayout.addDrawerListener(drawerToggle)
+
+        setupToolbar(showHamburger = true)
+        setupDrawerContent()
         initInstance()
 
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(drawerToggle.onOptionsItemSelected(item!!)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    private fun setupDrawerToggle() : ActionBarDrawerToggle {
+        return ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close)
+    }
+
+    private fun setupDrawerContent(){
+        nvView.setNavigationItemSelectedListener {
+            selectDrawerItem(it)
+            return@setNavigationItemSelectedListener true
+        }
+    }
+
+    private fun selectDrawerItem(menuItem: MenuItem){
+
+        var fragment: Fragment? = null
+
+        when(menuItem.itemId){
+            R.id.nav_home_fragment -> {
+                fragment = HomeFragment.newInstance("Home")
+            }
+            R.id.nav_point_fragment -> {
+                fragment = PointFragment.newInstance("Point")
+            }
+            R.id.nav_promotion_fragment -> {
+                fragment = PromotionFragment.newInstance("Promotion")
+            }
+            R.id.nav_setting_fragment -> {
+                fragment = SettingFragment.newInstance("Setting")
+
+            }
+            R.id.nav_about_fragment -> {
+                fragment = AboutFragment.newInstance("Home")
+            }
+        }
+
+        replaceFragment(fragment = fragment!!)
+
+        menuItem.isCheckable = true
+        title = menuItem.title
+        drawerLayout.closeDrawers()
+    }
+
     private fun initInstance() {
-        replaceFragment(fragment = PromotionFragment.newInstance("HomeFragment"))
+
     }
 }
---
+++ app/src/main/java/com/socket9/pointube/screens/setting/SettingFragment.kt
@@ -0,0 +1,59 @@
+package com.socket9.pointube.screens.setting
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.socket9.pointube.R
+
+/**
+ * Created by Euro (ripzery@gmail.com) on 3/10/16 AD.
+ */
+class SettingFragment : Fragment() {
+
+    /** Variable zone **/
+    lateinit var param1: String
+
+
+    /** Static method zone **/
+    companion object {
+        val ARG_1 = "ARG_1"
+
+        fun newInstance(param1: String): SettingFragment {
+            var bundle: Bundle = Bundle()
+            bundle.putString(ARG_1, param1)
+            val settingFragment: SettingFragment = SettingFragment()
+            settingFragment.arguments = bundle
+            return settingFragment
+        }
+
+    }
+
+    /** Activity method zone  **/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) {
+            /* if newly created */
+            param1 = arguments.getString(ARG_1)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView: View = inflater!!.inflate(R.layout.fragment_setting, container, false)
+
+        return rootView
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initInstance()
+    }
+
+    /** Method zone **/
+
+    private fun initInstance() {
+
+    }
+}
