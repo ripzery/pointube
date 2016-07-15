fc4f7ab7d8b9096432a3e3ffd223523a98eff65b
phuchit <ripzery@gmail.com>
2016-07-15 16:47:19 +0700

    Add extension for setup toolbar

--- app/src/main/java/com/socket9/pointube/extensions/Activities.kt
+++ app/src/main/java/com/socket9/pointube/extensions/Activities.kt
@@ -1,16 +1,25 @@
 package com.socket9.pointube.extensions
 
-import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import com.socket9.pointube.R
-import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
 /**
  * Created by Euro (ripzery@gmail.com) on 7/11/2016 AD.
  */
 
-fun AppCompatActivity.replaceFragment(container: Int = R.id.contentContainer, fragment: Fragment){
+fun AppCompatActivity.replaceFragment(container: Int = R.id.contentContainer, fragment: Fragment) {
     this.supportFragmentManager.beginTransaction().replace(container, fragment).commit()
+}
+
+fun AppCompatActivity.setupToolbar(title: String? = "Pointube", showHamburger: Boolean = false) {
+    setSupportActionBar(toolbar)
+    tvToolbarTitle.text = title
+    if (showHamburger) {
+
+    } else {
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 }
--- app/src/main/java/com/socket9/pointube/screens/MainActivity.kt
+++ app/src/main/java/com/socket9/pointube/screens/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.socket9.pointube.R
 import com.socket9.pointube.extensions.replaceFragment
+import com.socket9.pointube.extensions.setupToolbar
 import com.socket9.pointube.screens.home.HomeFragment
 import com.socket9.pointube.screens.promotion.main.PromotionFragment
 import org.jetbrains.anko.AnkoLogger
@@ -13,7 +14,9 @@ class MainActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupToolbar()
         initInstance()
+
     }
 
     private fun initInstance() {
