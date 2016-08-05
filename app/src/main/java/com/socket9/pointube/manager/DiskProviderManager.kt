32803b8059fddcf5f1a62635da689db208a07591
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-06 00:18:34 +0700

    Implement read getAllBrandMember & getAllBrandSelectMember from disk

--- app/src/main/java/com/socket9/pointube/manager/DiskProviderManager.kt
+++ app/src/main/java/com/socket9/pointube/manager/DiskProviderManager.kt
@@ -1,9 +1,12 @@
 package com.socket9.pointube.manager
 
 import com.socket9.pointube.repository.brands.BrandRepo
+import com.socket9.pointube.repository.brands.GetMemberBrand
+import com.socket9.pointube.repository.brands.GetMemberSelectBrand
 import com.socket9.pointube.repository.programs.PublishedProgramItemRepo
 import com.socket9.pointube.screens.home.HomeModel
 import com.socket9.pointube.utils.RealmUtil
+import io.realm.RealmResults
 import org.jetbrains.anko.AnkoLogger
 import rx.Observable
 
@@ -11,11 +14,23 @@ import rx.Observable
  * Created by Euro (ripzery@gmail.com) on 7/16/2016 AD.
  */
 object DiskProviderManager : AnkoLogger {
+    /* Brands */
     fun getAllProvider(): Observable<HomeModel.AllBrands> {
         val allBrands = RealmUtil.readAll(BrandRepo::class.java)
         return Observable.just(HomeModel.AllBrands(true, allBrands.toMutableList(), true))
     }
 
+    fun getAllBrandMember(): Observable<GetMemberBrand> {
+        val allBrands = RealmUtil.readAll(GetMemberBrand::class.java)
+        return Observable.just(allBrands.first())
+    }
+
+    fun getAllBrandSelectMember(): Observable<GetMemberSelectBrand>{
+        val allBrands = RealmUtil.readAll(GetMemberSelectBrand::class.java)
+        return Observable.just(allBrands.first())
+    }
+
+    /* Programs */
     fun getPublishedProgramList(): Observable<HomeModel.PublishedProgramListRepo> {
         val allPublishedProgramList = RealmUtil.readAll(PublishedProgramItemRepo::class.java)
         return Observable.just(HomeModel.PublishedProgramListRepo(true, null, allPublishedProgramList.toMutableList(), true))
@@ -44,4 +59,5 @@ object DiskProviderManager : AnkoLogger {
         val allPublishedProgramList = RealmUtil.readAll(PublishedProgramItemRepo::class.java)
         return Observable.just(allPublishedProgramList.filter { it.ProviderId == providerId }.toMutableList())
     }
+
 }
