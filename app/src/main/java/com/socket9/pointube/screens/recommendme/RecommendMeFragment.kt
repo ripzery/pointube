53de5e55cfc659a201132204faf2dd27e3d747b0
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-05 15:03:36 +0700

    Add toolbar for recommend me activity

--- app/src/main/java/com/socket9/pointube/screens/recommendme/RecommendMeFragment.kt
+++ app/src/main/java/com/socket9/pointube/screens/recommendme/RecommendMeFragment.kt
@@ -3,6 +3,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.socket9.pointube.R
 import org.jetbrains.anko.AnkoLogger
 
 /**
@@ -40,7 +41,7 @@ class RecommendMeFragment : Fragment(),AnkoLogger {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView: View = inflater!!.inflate(R.layout.fragment_template, container, false)
+        val rootView: View = inflater!!.inflate(R.layout.fragment_recommend_me, container, false)
 
         return rootView
     }
