322e6263878ac748312c352b12101f9092895e3a
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-08 17:47:12 +0700

    Add ThaiAirlineAwardActivity

---
+++ app/src/main/java/com/socket9/pointube/screens/thai_airline/ThaiAirlineAwardActivity.kt
@@ -0,0 +1,18 @@
+package com.socket9.pointube.screens.thai_airline
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.socket9.pointube.R
+
+class ThaiAirlineAwardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thai_airline_award)
+        initInstance()
+    }
+
+    private fun initInstance() {
+
+    }
+}
