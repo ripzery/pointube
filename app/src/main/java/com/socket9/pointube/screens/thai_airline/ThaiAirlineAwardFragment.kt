7b5c0c80f50e701d8bb51b3b8f56210c9ccb7758
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-08 17:50:07 +0700

    Add ThaiAirlineAwardFragment

---
+++ app/src/main/java/com/socket9/pointube/screens/thai_airline/ThaiAirlineAwardFragment.kt
@@ -0,0 +1,58 @@
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.socket9.pointube.R
+
+/**
+ * Created by ripzery on 7/20/16.
+ */
+
+class ThaiAirlineAwardFragment : Fragment() {
+
+    /** Variable zone **/
+    lateinit var param1: String
+
+
+    /** Static method zone **/
+    companion object {
+        val ARG_1 = "ARG_1"
+
+        fun newInstance(param1: String): ThaiAirlineAwardFragment {
+            var bundle: Bundle = Bundle()
+            bundle.putString(ARG_1, param1)
+            val templateFragment: ThaiAirlineAwardFragment = ThaiAirlineAwardFragment()
+            templateFragment.arguments = bundle
+            return templateFragment
+        }
+
+    }
+
+    /** Activity method zone  **/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) {
+            /* if newly created */
+            param1 = arguments.getString(ARG_1)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView: View = inflater!!.inflate(R.layout.fragment_thai_airline_award, container, false)
+
+        return rootView
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initInstance()
+    }
+
+    /** Method zone **/
+
+    private fun initInstance() {
+
+    }
+}
