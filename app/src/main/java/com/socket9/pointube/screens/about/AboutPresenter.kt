3d482a4584ab94698f7d1fcc81d0e8473f328ea2
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-08 12:49:06 +0700

    Create AboutPresenter and implement presenter contract

--- app/src/main/java/com/socket9/pointube/screens/about/AboutPresenter.kt
+++ app/src/main/java/com/socket9/pointube/screens/about/AboutPresenter.kt
@@ -3,3 +3,21 @@ package com.socket9.pointube.screens.about
 /**
  * Created by ripzery on 8/8/16.
  */
+class AboutPresenter(var view: AboutContract.View?) : AboutContract.Presenter {
+    override fun clickAboutDetail(position: Int) {
+        view?.showAboutDetail(position)
+    }
+
+    override fun loadAboutList() {
+        /*  TODO: create list and send to view*/
+    }
+
+    override fun onCreate() {
+
+    }
+
+    override fun onDestroy() {
+        view = null
+    }
+
+}
