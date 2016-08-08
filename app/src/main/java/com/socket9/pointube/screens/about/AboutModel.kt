d5ef2225ffa617a65ebca15fec0b38c1a7d890b0
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-08 12:35:03 +0700

    Create about model and set model in about viewgroup

---
+++ app/src/main/java/com/socket9/pointube/screens/about/AboutModel.kt
@@ -0,0 +1,9 @@
+package com.socket9.pointube.screens.about
+
+import android.support.annotation.LayoutRes
+
+/**
+ * Created by ripzery on 8/8/16.
+ */
+
+data class AboutItem(@LayoutRes val icon: Int, val title: String)
