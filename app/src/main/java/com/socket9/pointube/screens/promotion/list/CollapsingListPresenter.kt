e7feb376d5dbe4183dc8e6a0c203f05727dd18db
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-09 18:24:15 +0700

    Implement selectPromotionList in CollapsingListPresenter

--- app/src/main/java/com/socket9/pointube/screens/promotion/list/CollapsingListPresenter.kt
+++ app/src/main/java/com/socket9/pointube/screens/promotion/list/CollapsingListPresenter.kt
@@ -8,6 +8,19 @@ import org.jetbrains.anko.warn
  * Created by ripzery on 8/9/16.
  */
 class CollapsingListPresenter(var view: CollapsingListContract.View?) : CollapsingListContract.Presenter, AnkoLogger {
+    override fun selectPromotionList(brandId: Int) {
+        DataManager.getProviderById(brandId)
+                .subscribe({
+                    if (it.Name.equals("Royal Orchid Plus")) {
+                        view?.showThaiAirwayPromotionList()
+                    } else {
+                        view?.showDefaultPromotionList()
+                    }
+                }, {
+                    warn { it }
+                })
+    }
+
     override fun loadCover(brandId: Int) {
         DataManager.getProviderById(brandId)
                 .subscribe({
