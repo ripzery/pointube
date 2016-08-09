094d58406811c47c8840876a2373a05016f5095c
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-09 18:46:03 +0700

    Handle back button in thai airline award

--- app/src/main/java/com/socket9/pointube/screens/promotion/list/thai_airline/chart/ThaiAirlineAwardChartActivity.kt
+++ app/src/main/java/com/socket9/pointube/screens/promotion/list/thai_airline/chart/ThaiAirlineAwardChartActivity.kt
@@ -2,6 +2,7 @@ package com.socket9.pointube.screens.promotion.list.thai_airline.chart
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.socket9.pointube.R
 import com.socket9.pointube.extensions.replaceFragment
 import com.socket9.pointube.extensions.setupToolbar
@@ -12,6 +13,13 @@ class ThaiAirlineAwardChartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thai_airline_award)
         initInstance()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            android.R.id.home -> finish()
+        }
+        return true
     }
 
     private fun initInstance() {
