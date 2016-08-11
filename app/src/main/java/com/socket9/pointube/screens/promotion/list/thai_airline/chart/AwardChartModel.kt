cab479811fabd0e657f8d0787d3a2a9ab541372b
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-11 13:51:33 +0700

    Implement presenter contract in award chart presenter and create mockup award chart model

--- app/src/main/java/com/socket9/pointube/screens/promotion/list/thai_airline/chart/AwardChartModel.kt
+++ app/src/main/java/com/socket9/pointube/screens/promotion/list/thai_airline/chart/AwardChartModel.kt
@@ -3,4 +3,6 @@ package com.socket9.pointube.screens.promotion.list.thai_airline.chart
 /**
  * Created by ripzery on 8/10/16.
  */
-data class AwardChartModel(val title: String, val subtitle: String, val description: String)
+data class AwardChartModel(val title: String, val subtitle: String, val description: String, val priceRound: AwardPriceTrip, val priceOneWay: AwardPriceTrip)
+
+data class AwardPriceTrip(val economy: String, val premiumEconomy: String, val silkClass: String, val firstClass: String)
