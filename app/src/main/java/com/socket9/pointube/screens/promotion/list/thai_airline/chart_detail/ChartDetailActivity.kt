ed6498da666efe6640576c186cef39e4c141f9ea
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-11 15:57:36 +0700

    Implement chart detail activity

--- app/src/main/java/com/socket9/pointube/screens/promotion/list/thai_airline/chart_detail/ChartDetailActivity.kt
+++ app/src/main/java/com/socket9/pointube/screens/promotion/list/thai_airline/chart_detail/ChartDetailActivity.kt
@@ -2,6 +2,7 @@ package com.socket9.pointube.screens.promotion.list.thai_airline.chart_detail
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.socket9.pointube.R
 import com.socket9.pointube.extensions.replaceFragment
 import com.socket9.pointube.extensions.setupToolbar
@@ -12,6 +13,15 @@ class ChartDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chart_detail)
         initInstance()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+        return true
     }
 
     private fun initInstance() {
