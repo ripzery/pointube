0391a1b742d98b734adb11aff3ff44de32b00ace
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-11 16:54:54 +0700

    Implement loadChartDetail in chart detail presenter

--- app/src/main/java/com/socket9/pointube/screens/promotion/list/thai_airline/chart_detail/ChartDetailPagerItemPresenter.kt
+++ app/src/main/java/com/socket9/pointube/screens/promotion/list/thai_airline/chart_detail/ChartDetailPagerItemPresenter.kt
@@ -1,5 +1,23 @@
 package com.socket9.pointube.screens.promotion.list.thai_airline.chart_detail
 
+import com.socket9.pointube.manager.DataManager
+
 /**
  * Created by ripzery on 8/11/16.
  */
+class ChartDetailPagerItemPresenter(var view: ChartDetailPagerItemContract.View?) : ChartDetailPagerItemContract.Presenter {
+    override fun loadChartDetail(isRound: Boolean, position: Int) {
+        DataManager.getAirlineAwardChartList().subscribe {
+            view?.showChartList(if (isRound) it[position].priceRound else it[position].priceOneWay)
+        }
+    }
+
+    override fun onCreate() {
+
+    }
+
+    override fun onDestroy() {
+        view = null
+    }
+
+}
