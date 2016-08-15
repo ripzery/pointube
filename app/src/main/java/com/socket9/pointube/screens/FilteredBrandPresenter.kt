9e5bf98733cda433e4548082c0b1fb0008ca794a
Phuchit Sirimongkolsathien <ripzery@Phuchits-MacBook-Pro.local>
2016-08-15 16:59:27 +0700

    Get brand data in filtered brand presenter

--- app/src/main/java/com/socket9/pointube/screens/FilteredBrandPresenter.kt
+++ app/src/main/java/com/socket9/pointube/screens/FilteredBrandPresenter.kt
@@ -1,12 +1,20 @@
 package com.socket9.pointube.screens
 
+import com.socket9.pointube.manager.DataManager
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.warn
+
 /**
  * Created by ripzery on 8/15/16.
  */
-class FilteredBrandPresenter(var view: FilteredBrandContract.View?) : FilteredBrandContract.Presenter{
+class FilteredBrandPresenter(var view: FilteredBrandContract.View?) : FilteredBrandContract.Presenter, AnkoLogger {
     override fun loadAllBrands() {
-//        view?.onLoadAllBrands()
-
+        DataManager.getAllProvider()
+                .subscribe({
+                    view?.onLoadAllBrands(it.Results)
+                }, {
+                    warn { it }
+                })
     }
 
     override fun onCreate() {
